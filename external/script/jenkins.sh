c0bb0ab084b35df949f793a1e99d23d1d9aa601c
rockman88v <rockman88v@gmail.com>
2023-05-11 11:43:23 +0700

    init commit

---
+++ external/script/jenkins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo touch install-jenkins.log
+echo "xxxxx install jenkins start"
+echo "xxxxx install jenkins inprogress"
+echo "xxxxx install jenkins finish"
---
+++ self-manage-cluster/variables.tf
@@ -0,0 +1,54 @@
+variable "cluster_name" {
+  type        = string
+  description = "cluster name will be added as prefix for other resource to avoid resource name collision"
+}
+variable "keypair_name" {
+  type        = string
+  description = "Key pair name you wanted to assign to both control plane and worker node"
+}
+
+variable "instance_type_master" {
+  type        = string
+  description = "Instance type master"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for worker"
+}
+
+variable "region" {
+  type        = string
+  description = "Specific region for your account"
+}
+
+
+variable "control_plane_instance_name" {
+  type        = string
+  description = "Control Plane instance name"
+}
+
+variable "worker_name" {
+  type        = string
+  description = "Worker instance name"
+}
+
+variable "number_of_workers" {
+  type        = number
+  description = "Number of worker nodes that you want to create"
+  default     = 1
+}
+
+
+variable "include_policy_ebs_csi_driver" {
+  type        = bool
+  description = "This setting will add EBS CSI Policy. This is not recommend for learning Storage"
+  default     = false
+}
+
+
+variable "include" {
+  type        = list(any)
+  description = "This setting will install things at bootstrap. Allowed Values are: helm, etcd, ebs-csi"
+  default     = []
+}
